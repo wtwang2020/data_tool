413a03ff214fffcb9f7d9fea3fa50ffbcbd05396
wtwang2020 <93303045+wtwang2020@users.noreply.github.com>
2024-04-25 11:24:45 +0800

    Update 01. Cellranger_pipeline.sh

--- 单细胞工具模块/上游分析/cell_ranger_pipline/01. Cellranger_pipeline.sh
+++ 单细胞工具模块/上游分析/cell_ranger_pipline/01. Cellranger_pipeline.sh
@@ -1,31 +1,4 @@
-#Code script for cellranger pipeline which is executed on Linux System. 
-#Pipeline can be built accodrding to the tutorials provided by 10xgenomics at https://www.10xgenomics.com/support/software/cell-ranger/latest/tutorials.
-#Example is taken on the basis of adrenal sample 1 under FD condition  (FD_1_adrenal) which can be accessed and downloaded from GSA under CRA006610. 
-#/../bin/cellranger: cellranger installation location.
-#/../mm10: reference file location.
-#/../raw/adrenal: raw fastq files location.
----------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------
-################# 1. Download mm10 genome file and genome annotation file
-#################
-#################
-#################
-#################
-#################
-wget ftp://ftp.ensembl.org/pub/release-93/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
-gunzip Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
-
-wget ftp://ftp.ensembl.org/pub/release-93/gtf/mus_musculus/Mus_musculus.GRCm38.93.gtf.gz
-gunzip Mus_musculus.GRCm38.93.gtf.gz
-
-################# 2. Build mm10 reference for cellranger downstream analysis
-#################
-#################
-#################
-#################
-#################
-cellranger mkgtf Mus_musculus.GRCm38.93.gtf Mus_musculus.GRCm38.93.filtered.gtf \
+/home/wangweitao/wangweitao/learning/Single-cell-atlas-of-sex-differences-and-androgen-effects/cellranger/cellranger-8.0.0/cellranger mkgtf Mus_musculus.GRCm38.93.gtf Mus_musculus.GRCm38.93.filtered.gtf \
                  --attribute=gene_biotype:protein_coding \
                  --attribute=gene_biotype:lincRNA \
                  --attribute=gene_biotype:antisense \
@@ -44,7 +17,7 @@ cellranger mkgtf Mus_musculus.GRCm38.93.gtf Mus_musculus.GRCm38.93.filtered.gtf 
                  --attribute=gene_biotype:TR_J_pseudogene \
                  --attribute=gene_biotype:TR_C_gene
 
-cellranger mkref --genome=mm10 \
+/home/wangweitao/wangweitao/learning/Single-cell-atlas-of-sex-differences-and-androgen-effects/cellranger/cellranger-8.0.0/cellranger mkref --genome=mm10 \
                  --fasta=Mus_musculus.GRCm38.dna.primary_assembly.fa \
                  --genes=Mus_musculus.GRCm38.93.filtered.gtf \
                  --ref-version=3.0.0
@@ -56,11 +29,10 @@ cellranger mkref --genome=mm10 \
 #################
 #################
 # --fastqs=/../raw/adrenal \为原始数据的位置
-cellranger count --id=FD_1_adrenal \
+/home/wangweitao/wangweitao/learning/Single-cell-atlas-of-sex-differences-and-androgen-effects/cellranger/cellranger-8.0.0/cellranger count --id=FD_1_adrenal \
                    --transcriptome=/../mm10 \
                    --fastqs=/../raw/adrenal \
                    --sample=FD-1-adrenal \
                    --expect-cells=10000 \
                    --localcores=15 \
                    --localmem=64
-
